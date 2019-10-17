b42b591645eb9b40cc39a6e087557b80915343c2
haoxileaoi <haoxlei@outlook.com>
2019-10-17 13:55:12 +0800

    设置允许访问的host

--- blogproject/settings.py
+++ blogproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '5(u93s9ef&2bk)tij2^)5@ke2c&*&-jbpisxq5p+-z7m9z*tmo'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost ', '.www.haoxileaoi.com']
 
 
 # Application definition
@@ -120,3 +120,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
